222be168bbf0a7d26d7a129173751af3f7ffdd47
dirty-nine <nrwgalaxy@gmail.com>
2026-01-01 01:52:38 -0800

    first commit

---
+++ Recomend_Me.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.myanimelist.net/v2/anime?q=one&limit=5"
+headers = {
+    "X-MAL-CLIENT-ID": "YOUR_CLIENT_ID"
+}
+
+res = requests.get(url, headers=headers)
+data = res.json()
+
+print(data)
